0c23c2b6084ebce2f69baa451e5d1558f54021eb
sekito <sekito1107gg@gmail.com>
2024-05-08 01:05:33 +0900

    編集画面に現在の画像を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  validate :avatar_format
+
+  def avatar_format
+    return unless avatar.attached? && !avatar.content_type.in?(%w[image/jpeg image/png image/gif])
+
+    errors.add(:avatar, 'プロフィール画像はjpg,png,gifの中から選択してください')
+  end
 end
