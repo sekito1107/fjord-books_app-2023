47d9cd7887b180a314443c6fc2f5923bf5a8a724
sekito <sekito1107gg@gmail.com>
2024-05-16 03:29:42 +0900

    関連の記述を修正

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ReportMention < ApplicationRecord
-  belongs_to :source_report, class_name: 'Report', inverse_of: :active_mentions
-  belongs_to :target_report, class_name: 'Report', inverse_of: :passive_mentions
+  belongs_to :source_report, class_name: 'Report'
+  belongs_to :target_report, class_name: 'Report'
   validates :source_report, uniqueness: { scope: :target_report }
 end
